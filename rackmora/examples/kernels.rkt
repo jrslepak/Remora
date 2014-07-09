b471309c0e6ec9dcc754fddebf0490cd37b3149a
Justin Slepak <jrslepak@ccs.neu.edu>
2014-07-09 14:36:11 -0400

    Add small statistics and signal processing kernels

---
+++ rackmora/examples/kernels.rkt
@@ -0,0 +1,81 @@
+#lang rackmora
+(require racket/math)
+(require (rename-in (only-in racket/base + *)
+                    (+ R+)
+                    (* R*)))
+
+;; Mean
+(def mean
+  (fn ((samples 1)) (/ (foldr + 0 samples)
+                       (tally samples))))
+
+
+;; Variance
+(def variance
+  (fn ((samples 1))
+      (mean (sqr (- samples (mean samples))))))
+
+
+;; Covariance
+(def covariance
+  (fn ((xs 1) (ys 1))
+      (mean (* (- xs (mean xs))
+               (- ys (mean ys))))))
+
+
+;; Autocovariance -- covariance of a signal and a delayed version of itself
+;; For this example, we pad out the end of the delayed signal with samples
+;; from the beginning of the signal
+(def autocovariance
+  (fn ((samples 1) (delay 0))
+      (covariance samples (rotate samples delay))))
+
+
+;; Pearson correlation
+
+
+;; Autocorrelation
+;(def autocorrelation
+;  (fn ((signal 1) (delay 0))
+
+
+;; Convolution
+
+
+;; Generate a sinusoid with given (digital) frequency and phase
+(def sinusoid
+  (fn ((length 0) (freq 0) (phase 0))
+      (unbox count (iota [length])
+             (cos (+ (R* count freq 2 pi)
+                     phase)))))
+
+
+;; Goertzel algorithm (extract single frequency component)
+;; first stage is IIR, second is FIR
+;; Scan is used for demonstration purposes, but for selecting just the single
+;; DFT result, it may be better to use foldl, as only the final value of the
+;; accumulator is actually needed.
+(def goertzel-iir-step
+  (fn ((freq 0))
+      (fn ((next 0) (accum 1))
+          (array (- (+ next (R* 2 (cos (R* 2 pi freq)) (head accum)))
+                    (tail accum))
+                 (head accum)))))
+(def goertzel-iir
+  (fn ((freq 0) (signal 1))
+      (#r(1)head (scan (goertzel-iir-step freq) (array 0 0) signal))))
+(def goertzel-fir-step
+  (fn ((freq 0) (win 1)) ; length-2 window of post-IIR signal
+      (- (tail win)
+         (* (head win) (exp (- 0 (* 2 (* pi (* 0+i freq)))))))))
+(def goertzel-fir
+  (fn ((freq 0) (post-iir 1))
+      (goertzel-fir-step freq (unsafe-unbox (take-right 2 post-iir)))))
+(def goertzel
+  (fn ((freq 0) (signal 1))
+      ; magnitude must be scaled down by half the buffer length
+      ; result phase is how far from 0 (i.e., 2π) the buffer's last sample is
+      (/ (goertzel-fir freq (goertzel-iir freq signal))
+         (/ (tally signal) 2))))
+
+
